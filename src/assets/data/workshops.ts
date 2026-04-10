ee1adc173c96ec41a159ad4cb60e8ce9ba471c98
Soma Veszelovszki <soma.veszelovszki@tulip.co>
2026-04-10 21:49:04 +0200

    Workshops page in progress

--- src/assets/data/workshops.ts
+++ src/assets/data/workshops.ts
@@ -51,106 +51,57 @@ export const workshops = [
     ]
   }
 ]
-export type WorkshopPortfolio = {
-  slug: string
+
+export type PastWorkshopHighlight = {
   title: string
-  caption: string
-  date: string
-  location: string
-  coverImage: string
+  description: string
   images: string[]
-  facebookEventUrl: string
-  registrationUrl: string
+}
+
+export type WorkshopDate = {
+  date: string
+  time: string
 }
 
 export type UpcomingWorkshop = {
+  slug: string
   title: string
-  date: string
-  time: string
-  location: string
   description: string
+  coverImage: string
+  images: string[]
+  dates: WorkshopDate[]
+  location: string
+  price: string
   facebookEventUrl: string
   registrationUrl: string
 }
 
-export const workshopPortfolios: WorkshopPortfolio[] = [
-  {
-    slug: 'adventi-koszoru-2025',
-    title: 'Adventi koszorú workshop',
-    caption: 'Közösen készítettünk természetes adventi koszorúkat gyertyafényes, meghitt hangulatban.',
-    date: '2025. november 30.',
-    location: 'Budapest, Mimoza Studio',
-    coverImage: '/weddings/dori-david/dori-david-3.jpg',
-    images: [
-      '/weddings/dori-david/dori-david-1.jpg',
-      '/weddings/dori-david/dori-david-2.jpg',
-      '/weddings/dori-david/dori-david-3.jpg',
-      '/weddings/dori-david/dori-david-4.jpg'
-    ],
-    facebookEventUrl: 'https://www.facebook.com/events/1196509952087690',
-    registrationUrl: 'https://forms.gle/cMcf8fE8gG9ztX9o6'
-  },
-  {
-    slug: 'mezei-csokor-2025',
-    title: 'Mezei csokorkötő workshop',
-    caption: 'Vadvirágos, laza kötésű csokrokat készítettünk szezonális alapanyagokból.',
-    date: '2025. június 15.',
-    location: 'Pécs, KertMűhely',
-    coverImage: '/weddings/reka-balint/reka-balint-5.jpg',
-    images: [
-      '/weddings/reka-balint/reka-balint-1.jpg',
-      '/weddings/reka-balint/reka-balint-3.jpg',
-      '/weddings/reka-balint/reka-balint-5.jpg',
-      '/weddings/reka-balint/reka-balint-6.jpg'
-    ],
-    facebookEventUrl: 'https://www.facebook.com/events/887430266928277',
-    registrationUrl: 'https://forms.gle/nrYCHu68baxF6KLd8'
-  },
-  {
-    slug: 'asztaldisz-2025',
-    title: 'Szezonális asztaldísz workshop',
-    caption: 'Textúrák, színpaletta és kompozíció: mindenki saját asztaldísszel távozott.',
-    date: '2025. március 9.',
-    location: 'Győr, Közösségi Ház',
-    coverImage: '/weddings/anna-bence/anna-bence-2.jpg',
-    images: [
-      '/weddings/anna-bence/anna-bence-1.jpg',
-      '/weddings/anna-bence/anna-bence-2.jpg',
-      '/weddings/anna-bence/anna-bence-3.jpg',
-      '/weddings/anna-bence/anna-bence-4.jpg'
-    ],
-    facebookEventUrl: 'https://www.facebook.com/events/1043388351226501',
-    registrationUrl: 'https://forms.gle/SHYDPomUUm8n3uZt8'
-  }
-]
+export const pastWorkshopHighlight: PastWorkshopHighlight = {
+  title: 'Korábbi workshopjaink',
+  description: 'Ízelítő a korábbi workshopjainkból, ahol közösen alkottunk, tanultunk és jól éreztük magunkat.',
+  images: [
+    '/workshops/adventi-koszoru-workshop/adventi-koszoru-workshop-1.jpg',
+    '/workshops/adventi-koszoru-workshop/adventi-koszoru-workshop-2.jpg',
+    '/workshops/adventi-koszoru-workshop/adventi-koszoru-workshop-3.jpg',
+    '/workshops/adventi-koszoru-workshop/adventi-koszoru-workshop-4.jpg'
+  ]
+}
 
 export const upcomingWorkshops: UpcomingWorkshop[] = [
   {
-    title: 'Tavaszi virágkoszorú workshop',
-    date: '2026. április 18.',
-    time: '10:00-13:00',
-    location: 'Budapest, Mimoza Studio',
-    description: 'Alap technikák, színpárosítás és egy kész koszorú, amit haza is vihetsz.',
+    slug: 'anyak-napi-viragbura-workshop',
+    title: 'Anyák napi virágbúra workshop',
+    description: 'Készíts egyedi virágbúrát édesanyádnak vagy magadnak egy kreatív, inspiráló délutánon.',
+    coverImage: '/workshops/anyak-napi-viragbura-workshop/anyak-napi-viragbura-workshop-profile.jpg',
+    images: [
+      '/workshops/anyak-napi-viragbura-workshop/anyak-napi-viragbura-workshop-profile.jpg',
+      '/workshops/anyak-napi-viragbura-workshop/anyak-napi-viragbura-workshop-1.jpg',
+      '/workshops/anyak-napi-viragbura-workshop/anyak-napi-viragbura-workshop-2.jpg'
+    ],
+    dates: [{ date: '2026. május 2.', time: '13:00-17:00' }],
+    location: 'LeonArt Stúdió - 2890 Tata, Egység u. 7.',
+    price: '15 000 Ft',
     facebookEventUrl: 'https://www.facebook.com/events/2549805432031408',
     registrationUrl: 'https://forms.gle/7MFQ8WJa2kL5wQ2g7'
-  },
-  {
-    title: 'Nyári csokorkötő alapok',
-    date: '2026. június 6.',
-    time: '14:00-17:00',
-    location: 'Pécs, KertMűhely',
-    description: 'Kezdőbarát csokorkötés mezei virágokkal, gyakorlati mini feladatokkal.',
-    facebookEventUrl: 'https://www.facebook.com/events/899513102376120',
-    registrationUrl: 'https://forms.gle/ebQXfSQY8mRgT8P57'
-  },
-  {
-    title: 'Őszi asztaldísz és gyertyakompozíció',
-    date: '2026. szeptember 20.',
-    time: '11:00-14:00',
-    location: 'Győr, Közösségi Ház',
-    description: 'Rétegezés, arányok és tartósabb megoldások az őszi enteriőrökhöz.',
-    facebookEventUrl: 'https://www.facebook.com/events/3441097890311877',
-    registrationUrl: 'https://forms.gle/5bJ2pbMdE3QvY8PB9'
   }
 ]
-
